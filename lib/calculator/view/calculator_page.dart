29af1480827757f36208ac75ff80027f4ad1c81c
Rodrigo Castro <rodrigo.castro@avira.com>
2021-12-09 22:35:34 +0100

    add calculator buttons test

--- lib/calculator/view/calculator_page.dart
+++ lib/calculator/view/calculator_page.dart
@@ -40,42 +40,66 @@ class CalculatorView extends StatelessWidget {
               crossAxisCount: 4,
               children: [
                 TextButton(
-                  child: const Text('7'),
+                  child: const Text(
+                    '7',
+                    key: Key('key_seven'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.seven),
                 ),
                 TextButton(
-                  child: const Text('8'),
+                  child: const Text(
+                    '8',
+                    key: Key('key_eight'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.eight),
                 ),
                 TextButton(
-                  child: const Text('9'),
+                  child: const Text(
+                    '9',
+                    key: Key('key_nine'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.nine),
                 ),
                 TextButton(
-                  child: const Text('/'),
+                  child: const Text(
+                    '/',
+                    key: Key('key_division'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.division),
                 ),
                 TextButton(
-                  child: const Text('4'),
+                  child: const Text(
+                    '4',
+                    key: Key('key_four'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.four),
                 ),
                 TextButton(
-                  child: const Text('5'),
+                  child: const Text(
+                    '5',
+                    key: Key('key_five'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.five),
                 ),
                 TextButton(
-                  child: const Text('6'),
+                  child: const Text(
+                    '6',
+                    key: Key('key_six'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.six),
                 ),
                 TextButton(
-                  child: const Text('*'),
+                  child: const Text(
+                    '*',
+                    key: Key('key_multiplication'),
+                  ),
                   onPressed: () => context
                       .read<CalculatorCubit>()
                       .type(Input.multiplication),
@@ -89,42 +113,67 @@ class CalculatorView extends StatelessWidget {
                       context.read<CalculatorCubit>().type(Input.one),
                 ),
                 TextButton(
-                  child: const Text('2'),
+                  child: const Text(
+                    '2',
+                    key: Key('key_two'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.two),
                 ),
                 TextButton(
-                  child: const Text('3'),
+                  child: const Text(
+                    '3',
+                    key: Key('key_three'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.three),
                 ),
                 TextButton(
-                  child: const Text('+'),
+                  child: const Text(
+                    '+',
+                    key: Key('key_addition'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.addition),
                 ),
                 TextButton(
-                  child: const Text('0'),
+                  child: const Text(
+                    '0',
+                    key: Key('key_zero'),
+                    semanticsLabel: 'zero',
+                  ),
                   onPressed: () =>
-                      context.read<CalculatorCubit>().type(Input.seven),
+                      context.read<CalculatorCubit>().type(Input.zero),
                 ),
                 TextButton(
-                  child: const Text('.'),
+                  child: const Text(
+                    '.',
+                    key: Key('key_decimalPoint'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.decimalPoint),
                 ),
                 TextButton(
-                  child: const Text('='),
+                  child: const Text(
+                    '=',
+                    key: Key('key_result'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.result),
                 ),
                 TextButton(
-                  child: const Text('-'),
+                  child: const Text(
+                    '-',
+                    key: Key('key_subtraction'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.subtraction),
                 ),
                 TextButton(
-                  child: const Text('AC'),
+                  child: const Text(
+                    'AC',
+                    key: Key('key_allClear'),
+                  ),
                   onPressed: () =>
                       context.read<CalculatorCubit>().type(Input.allClear),
                 ),
--- test/calculator/view/calculator_page_test.dart
+++ test/calculator/view/calculator_page_test.dart
@@ -40,6 +40,25 @@ void main() {
       expect(find.text('$state'), findsOneWidget);
     });
 
+    testWidgets(
+      'perform key 0 on tapping button 0',
+      (tester) async {
+        when(() => calculatorCubit.state).thenReturn(0);
+        when(() => calculatorCubit.type(Input.zero)).thenReturn(null);
+
+        await tester.pumpApp(
+          BlocProvider.value(
+            value: calculatorCubit,
+            child: const CalculatorView(),
+          ),
+        );
+
+        await tester.tap(find.bySemanticsLabel('zero'));
+        verify(() => calculatorCubit.type(Input.zero)).called(1);
+      },
+      skip: true,
+    );
+
     testWidgets('perform key 1 on tapping button 1', (tester) async {
       when(() => calculatorCubit.state).thenReturn(0);
       when(() => calculatorCubit.type(Input.one)).thenReturn(null);
@@ -54,5 +73,246 @@ void main() {
       await tester.tap(find.byKey(const Key('key_one')));
       verify(() => calculatorCubit.type(Input.one)).called(1);
     });
+
+    testWidgets('perform key 2 on tapping button 2', (tester) async {
+      when(() => calculatorCubit.state).thenReturn(0);
+      when(() => calculatorCubit.type(Input.two)).thenReturn(null);
+
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: calculatorCubit,
+          child: const CalculatorView(),
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('key_two')));
+      verify(() => calculatorCubit.type(Input.two)).called(1);
+    });
+
+    testWidgets('perform key 3 on tapping button 3', (tester) async {
+      when(() => calculatorCubit.state).thenReturn(0);
+      when(() => calculatorCubit.type(Input.three)).thenReturn(null);
+
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: calculatorCubit,
+          child: const CalculatorView(),
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('key_three')));
+      verify(() => calculatorCubit.type(Input.three)).called(1);
+    });
+
+    testWidgets('perform key 4 on tapping button 4', (tester) async {
+      when(() => calculatorCubit.state).thenReturn(0);
+      when(() => calculatorCubit.type(Input.four)).thenReturn(null);
+
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: calculatorCubit,
+          child: const CalculatorView(),
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('key_four')));
+      verify(() => calculatorCubit.type(Input.four)).called(1);
+    });
+
+    testWidgets('perform key 5 on tapping button 5', (tester) async {
+      when(() => calculatorCubit.state).thenReturn(0);
+      when(() => calculatorCubit.type(Input.five)).thenReturn(null);
+
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: calculatorCubit,
+          child: const CalculatorView(),
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('key_five')));
+      verify(() => calculatorCubit.type(Input.five)).called(1);
+    });
+
+    testWidgets('perform key 6 on tapping button 6', (tester) async {
+      when(() => calculatorCubit.state).thenReturn(0);
+      when(() => calculatorCubit.type(Input.six)).thenReturn(null);
+
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: calculatorCubit,
+          child: const CalculatorView(),
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('key_six')));
+      verify(() => calculatorCubit.type(Input.six)).called(1);
+    });
+
+    testWidgets('perform key 7 on tapping button 7', (tester) async {
+      when(() => calculatorCubit.state).thenReturn(0);
+      when(() => calculatorCubit.type(Input.seven)).thenReturn(null);
+
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: calculatorCubit,
+          child: const CalculatorView(),
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('key_seven')));
+      verify(() => calculatorCubit.type(Input.seven)).called(1);
+    });
+
+    testWidgets('perform key 8 on tapping button 8', (tester) async {
+      when(() => calculatorCubit.state).thenReturn(0);
+      when(() => calculatorCubit.type(Input.eight)).thenReturn(null);
+
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: calculatorCubit,
+          child: const CalculatorView(),
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('key_eight')));
+      verify(() => calculatorCubit.type(Input.eight)).called(1);
+    });
+
+    testWidgets('perform key 9 on tapping button 9', (tester) async {
+      when(() => calculatorCubit.state).thenReturn(0);
+      when(() => calculatorCubit.type(Input.nine)).thenReturn(null);
+
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: calculatorCubit,
+          child: const CalculatorView(),
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('key_nine')));
+      verify(() => calculatorCubit.type(Input.nine)).called(1);
+    });
+
+    testWidgets('perform key + on tapping button +', (tester) async {
+      when(() => calculatorCubit.state).thenReturn(0);
+      when(() => calculatorCubit.type(Input.addition)).thenReturn(null);
+
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: calculatorCubit,
+          child: const CalculatorView(),
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('key_addition')));
+      verify(() => calculatorCubit.type(Input.addition)).called(1);
+    });
+
+    testWidgets(
+      'perform key - on tapping button -',
+      (tester) async {
+        when(() => calculatorCubit.state).thenReturn(0);
+        when(() => calculatorCubit.type(Input.subtraction)).thenReturn(null);
+
+        await tester.pumpApp(
+          BlocProvider.value(
+            value: calculatorCubit,
+            child: const CalculatorView(),
+          ),
+        );
+
+        await tester.tap(find.byKey(const Key('key_subtraction')));
+        verify(() => calculatorCubit.type(Input.subtraction)).called(1);
+      },
+      skip: true,
+    );
+
+    testWidgets('perform key * on tapping button *', (tester) async {
+      when(() => calculatorCubit.state).thenReturn(0);
+      when(() => calculatorCubit.type(Input.multiplication)).thenReturn(null);
+
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: calculatorCubit,
+          child: const CalculatorView(),
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('key_multiplication')));
+      verify(() => calculatorCubit.type(Input.multiplication)).called(1);
+    });
+
+    testWidgets('perform key / on tapping button /', (tester) async {
+      when(() => calculatorCubit.state).thenReturn(0);
+      when(() => calculatorCubit.type(Input.division)).thenReturn(null);
+
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: calculatorCubit,
+          child: const CalculatorView(),
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('key_division')));
+      verify(() => calculatorCubit.type(Input.division)).called(1);
+    });
+
+    testWidgets(
+      'perform key . on tapping button .',
+      (tester) async {
+        when(() => calculatorCubit.state).thenReturn(0);
+        when(() => calculatorCubit.type(Input.decimalPoint)).thenReturn(null);
+
+        await tester.pumpApp(
+          BlocProvider.value(
+            value: calculatorCubit,
+            child: const CalculatorView(),
+          ),
+        );
+
+        await tester.tap(find.byKey(const Key('key_decimalPoint')));
+        verify(() => calculatorCubit.type(Input.decimalPoint)).called(1);
+      },
+      skip: true,
+    );
+
+    testWidgets(
+      'perform key AC on tapping button AC',
+      (tester) async {
+        when(() => calculatorCubit.state).thenReturn(0);
+        when(() => calculatorCubit.type(Input.allClear)).thenReturn(null);
+
+        await tester.pumpApp(
+          BlocProvider.value(
+            value: calculatorCubit,
+            child: const CalculatorView(),
+          ),
+        );
+
+        await tester.tap(find.byKey(const Key('key_allClear')));
+        verify(() => calculatorCubit.type(Input.allClear)).called(1);
+      },
+      skip: true,
+    );
+
+    testWidgets(
+      'perform key = on tapping button =',
+      (tester) async {
+        when(() => calculatorCubit.state).thenReturn(0);
+        when(() => calculatorCubit.type(Input.result)).thenReturn(null);
+
+        await tester.pumpApp(
+          BlocProvider.value(
+            value: calculatorCubit,
+            child: const CalculatorView(),
+          ),
+        );
+
+        await tester.tap(find.byKey(const Key('key_result')));
+        verify(() => calculatorCubit.type(Input.result)).called(1);
+      },
+      skip: true,
+    );
   });
 }
